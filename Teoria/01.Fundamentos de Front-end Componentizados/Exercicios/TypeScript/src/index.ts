d1adcabd5f84ad106d5dc2c88a44b5ec3948e97a
andsilva515 <anderson.ssilva@outlook.com>
2024-08-13 16:06:29 -0300

    exercicios typescript

--- Teoria/01.Fundamentos de Front-end Componentizados/Exercicios/TypeScript/src/index.ts
+++ Teoria/01.Fundamentos de Front-end Componentizados/Exercicios/TypeScript/src/index.ts
@@ -3,12 +3,14 @@ type heroi = {
     vulgo: string;
 };
 
+
 function printaObjetos(pessoa: heroi) {
     console.log(pessoa);
 }
 
-//isso printa um objeto
+
 printaObjetos({
     name: "bruce wayne",
     vulgo: "batman",
 });
+
